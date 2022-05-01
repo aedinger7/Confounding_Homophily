b9a0f45cff7380164f29df0abc7d4344814b7c2b
Andy Edinger <aedinge@iu.edu>
2022-05-01 15:47:35 -0400

    init

---
+++ asymmetry_updated.R
@@ -0,0 +1,80 @@
+# From https://www.stat.cmu.edu/~cshalizi/homophily-confounding/asymmetry-act.R
+# This has been slightly modified from the original code to fix some bugs and run smoothly
+
+
+#Simulate the asymmetric issue.
+asymmetry.sim <- function (num.nodes=400, scale=3, offset=0,
+                           y.noise=0.02, friend.noms=1,
+                           response="linear", nominate.by="distance",
+                           time.trend=0.3) {
+  # num.nodes=400; scale=3; offset=0; y.noise=0.02; response="not-linear"; nominate.by="not-distance";friend.noms=1; time.trend=0.3
+  
+  #covariates.
+  invlogit <- function(cc) exp(cc)/(1+exp(cc))
+  xx <- runif(num.nodes)
+  distances <- as.matrix(dist(xx, diag=TRUE, upper=TRUE))
+  
+  #potential friends.
+  adj.probs <- array(rbinom(length(distances), 1, invlogit(offset-scale*distances)), dim(distances))
+  diag(adj.probs) <- 0
+  adj.probs[lower.tri(adj.probs)] <- t(adj.probs)[lower.tri(adj.probs)]
+  
+  fixed.x.dist <- distances
+  
+  diag(fixed.x.dist) <- Inf
+  nominees <- t(rbind(sapply(1:num.nodes, FUN=function(ii) {
+    possibles <- which(adj.probs[ii,] == 1)
+    return(ifelse(rep(nominate.by=="distance",friend.noms),
+                  possibles[which(order(fixed.x.dist[ii,possibles])<=friend.noms)],
+                  sample(possibles, size=friend.noms, prob=invlogit(-abs((xx[possibles] - 0.5))))
+    ))
+  })))
+  #num.nodes-by-friend.nom should be the size of the object.
+  
+  #true adjacency matrix.
+  aa.mat <- array(0, dim(distances))
+  for (ii in 1:num.nodes) aa.mat[ii, nominees[ii,]] <- 1
+  #backwards matrix.
+  rev.aa.mat <- t(aa.mat)
+  #reciprocated matrix.
+  #recip.aa.mat <- aa.mat*rev.aa.mat
+  
+  y1 = (xx-0.5)^3+rnorm(num.nodes,0,y.noise)
+  y2 = y1+rnorm(num.nodes,time.trend*xx,y.noise)
+  
+  infl.y1 <- aa.mat%*%y1
+  back.y1 <- rev.aa.mat%*%y1
+  #recip.y1 <- recip.aa.mat%*%y1
+  
+  XX <- cbind(1, y1, infl.y1,  back.y1)
+
+  
+  trial <- lm(y2 ~ y1 + infl.y1 + back.y1)
+
+  coef.table <- summary(trial)$coefficients[,1]
+
+  v.plus.c <- c(diag(summary(trial)$cov.unscaled), summary(trial)$cov.unscaled[3,4])*summary(trial)$sigma^2
+  z.stat <- (coef.table[3]-coef.table[4])/sqrt(v.plus.c[3]+v.plus.c[4]+2*v.plus.c[5])
+  
+  out <- cbind(c(coef.table, v.plus.c, z.stat))
+  rownames(out) <- c("int.b", "auto.b", "infl.b", "back.b",
+                     "int.s", "auto.s", "infl.s", "back.s",
+                     "cov.infl.back",
+                     "z.stat")
+  
+  
+  return(out)
+  
+}
+
+asymmetry.sim()
+
+result <- replicate(5000, asymmetry.sim(friend=1, time.trend=0.3, nominate.by="distance"))
+
+dim(result)
+
+# pdf("timeseriesmodel-act-5000.pdf", width=12, height=6)
+par(mfrow=c(1,2))
+hist(result[3,,], main="Effect of Phantom `Influencer' on `Influenced' in Time Series", xlab="Regression Coefficient"); 
+hist(result[10,,], main="z-score of Directional Difference", xlab=paste("Proportion greater than 0:", mean(result[10,,]>0)))
+dev.off()
---
+++ neutral-diffusion_original.R
@@ -0,0 +1,99 @@
+#ACT's attempt at the voter model diffusion problem.
+
+library(ElectroGraph)
+
+create.network <- function(nodes=100, homoph.factor=1,
+                           baseline.density=0.01,
+                           same.boost=0.05) {
+  
+  prop <- rbinom(nodes, 1, 0.5)
+  prob.matrix <- 1*(array(prop,c(nodes,nodes)) == t(array(prop,c(nodes,nodes))))*same.boost + baseline.density
+  sociomat <- array(rbinom(length(prob.matrix), 1, prob.matrix), dim(prob.matrix))
+  sociomat <- 1*(sociomat+t(sociomat)>0)
+  diag(sociomat) <- 0
+  
+  return(list(sociomatrix=sociomat, traits=prop))
+}
+
+
+evolve.network <- function(sociomatrix, choice=rbinom(dim(sociomatrix)[1], 1, 0.5),
+                           save.points=1000, iterations.per.save=10) {
+  
+  nn <- dim(sociomatrix)[1]
+  choice.out <- array(NA, c(nn, save.points))
+  for (kk in 1:(save.points*iterations.per.save)) {
+    pick <- sample(nn, 1)
+    choice[pick] <- sample(choice[which(sociomatrix[pick,]==1)],1)
+    if (kk %% iterations.per.save == 0) choice.out[,kk/iterations.per.save] <- choice
+  }
+  return(choice.out)
+}
+
+
+
+nettest <- create.network(baseline.density=0.002)
+diag(nettest$socio) <- 0
+plot.obj <- electrograph(nettest$socio)
+initial.positions <- plot(plot.obj, label.cex=0.1, max.t=0.5, edge.color.f=c("gray","blue","red"))
+
+plot(plot.obj, node.colors=1+nettest$traits, manual.coord=initial.positions$coordinates,
+     label.cex=0.1, max.t=0.5, edge.color.f=c("gray","blue","red"))
+save(nettest, plot.obj, initial.positions, file="dont-lose-this.RData")
+
+
+initial.choice <- rbinom(dim(nettest$socio)[1], 1, 0.5)
+pdf("diffusion-initial-act.pdf", width=10, height=6)
+plot(plot.obj, node.colors=1+initial.choice, manual.coord=initial.positions$coordinates,
+     label.cex=0.1, max.t=0.5, edge.color.f=c("gray","blue","red"), component.bor=0)
+dev.off()
+
+choice.prog <- evolve.network(nettest$socio, choice=initial.choice)
+
+
+latest.choice <- choice.prog[,1000]
+
+pdf("diffusion-midway-act.pdf", width=10, height=6)
+plot(plot.obj, node.colors=1+choice.prog[,297], manual.coord=initial.positions$coordinates,
+     label.cex=0.1, max.t=0.5, edge.color.f=c("gray","blue","red"), component.bor=0)
+dev.off()
+
+
+
+
+
+#Density of current plot?
+dens <- mean(nettest$socio)
+test.zero <- array(rbinom(length(nettest$socio), 1, dens/2), dim(nettest$socio))
+test.zero <- test.zero + t(test.zero)
+
+choice.zero <- evolve.network(test.zero, choice=initial.choice)
+
+prog.effect <- zero.effect <- array(NA, c(2,dim(choice.prog)[2]))
+for (kk in 1:dim(choice.prog)[2]) {
+  res.e <- glm(choice.prog[,kk] ~ nettest$trait, family=binomial(link=logit))
+  prog.effect[,kk] <- summary(res.e)$coef[2,1:2]
+  
+  res.z <- glm(choice.zero[,kk] ~ nettest$trait, family=binomial(link=logit))
+  zero.effect[,kk] <- summary(res.z)$coef[2,1:2]
+}
+
+prog.e <- prog.effect[,1:100]
+zero.e <- zero.effect[,1:100]
+lims <- range(c(prog.e[1,]-2*prog.e[2,], prog.e[1,]+2*prog.e[2,],
+                zero.e[1,]-2*zero.e[2,], zero.e[1,]+2*zero.e[2,]))
+
+pdf("correlation-homoph.pdf", width=10, height=5)
+plot(c(0,1000), lims, ty="n", main="Homophilous Network", xlab="Step", ylab="Effect Size")
+x.seq <- seq(10, 1000, by=10)
+points(x.seq, prog.e[1,], col=2, pch=19)
+segments(x.seq, prog.e[1,]-2*prog.e[2,], x.seq, prog.e[1,]+2*prog.e[2,], col=2)
+abline(h=0, col=4)
+dev.off()
+
+pdf("correlation-neutral.pdf", width=10, height=5)
+plot(c(0,1000), lims, ty="n", main="Neutral-Model Network", xlab="Step", ylab="Effect Size")
+points(x.seq, zero.e[1,], col=1, pch=19)
+segments(x.seq, zero.e[1,]-2*zero.e[2,], x.seq, zero.e[1,]+2*zero.e[2,], col=1)
+abline(h=0, col=4)
+dev.off()
+
